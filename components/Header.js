c8022e77bee0c5953816324dbc0ca9fb3008e083
Elbarae1921 <elbarae1921@gmail.com>
2023-03-03 11:35:45 +0100

    feat: update icons

--- components/Header.js
+++ components/Header.js
@@ -7,10 +7,18 @@ export default function Header({ name }) {
       <Link href="/">
         <div className="relative w-12 h-12 block mx-auto mb-4">
           <Image
-            src="https://elbarae.me/mstile-70x70.png"
+            src="https://elbarae.me/blog-icon-dark.png"
             alt="Elbarae"
             fill
             style={{ objectFit: 'contain' }}
+            className="dark:hidden"
+          />
+          <Image
+            src="https://elbarae.me/blog-icon.png"
+            alt="Elbarae"
+            fill
+            style={{ objectFit: 'contain' }}
+            className="hidden dark:block"
           />
         </div>
         <p className="text-2xl dark:text-white text-center">{name}</p>
